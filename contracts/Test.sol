71cfb988705d0b3b26134d597ba1a1d178d9f6e9
Jacob Homanics <32080359+Hotmanics@users.noreply.github.com>
2022-12-06 13:24:31 -0600

    added first iteration of the golden hardhat project

---
+++ contracts/Test.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Test {
+    
+    function sayHello() public pure returns(string memory) {
+        return "hello";
+    }
+}
